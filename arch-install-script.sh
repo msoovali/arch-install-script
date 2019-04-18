801100655b326940c634d662e657eea228a0f786
Martin Sooväli <msoovali@gmail.com>
2019-04-18 10:05:23 +0300

    some changes

--- arch-install-script.sh
+++ arch-install-script.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-genfstab -U /mnt >> /mnt/etc/fstab
-
-echo Enter device's hostname:
+echo 'Enter device's hostname:'
 read $hostname
-
-arch-chroot /mnt
 
 ln -sf /usr/share/zoneinfo/Europe/Tallinn /etc/localtime
 hwclock --systohc
 
-sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8' /etc/locale.gen
-sed -i 's/#et_EE.UTF-8 UTF-8/et_EE.UTF-8 UTF-8' /etc/locale.gen
+sed -i 's/#en_US.UTF-8\ UTF-8/en_US.UTF-8\ UTF-8' /etc/locale.gen
+sed -i 's/#et_EE.UTF-8\ UTF-8/et_EE.UTF-8\ UTF-8' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
