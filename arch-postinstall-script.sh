18f37ac42441440075c6242d27c37f10c2e80cff
Martin Sooväli <msoovali@gmail.com>
2019-06-29 17:12:58 +0300

    add qemu and virtual-manager packages

--- arch-postinstall-script.sh
+++ arch-postinstall-script.sh
@@ -6,8 +6,10 @@ sudo pacman -S xorg-server xorg-apps xorg-xinit xf86-video-intel xf86-video-nouv
 # Display manager with gtk frontend and enable in systemd
 sudo pacman -S lightdm lightdm-gtk-greeter --noconfirm --needed
 sudo systemctl enable lightdm.service
-# graphical indicator for networkmanager
+# graphical indicator for networkmanager (not needed for cinnamon/gnome)
 sudo pacman -S network-manager-applet --noconfirm --needed
+# cinnamon desktop environment
+sudo pacman -S cinnamon gnome-terminal gedit --noconfirm --needed
 # mate desktop environment
 sudo pacman -S mate mate-extra --noconfirm --needed
 # printing service
@@ -16,7 +18,7 @@ sudo systemctl enable org.cups.cupsd.service
 # some fonts
 sudo pacman -S adobe-source-sans-pro-fonts cantarell-fonts noto-fonts terminus-font ttf-bitstream-vera ttf-dejavu ttf-droid ttf-inconsolata ttf-liberation ttf-roboto ttf-ubuntu-font-family tamsyn-font --noconfirm --needed
 # some useful applications
-sudo pacman -S firefox gimp audacity transmission-gtk vlc --noconfirm --needed
+sudo pacman -S firefox gimp audacity transmission-gtk vlc keepassxc libreoffice-still htop code --noconfirm --needed
 # wireless driver
 sudo pacman -S broadcom-wl --noconfirm --needed
 # themes
@@ -25,3 +27,6 @@ sudo pacman -S arc-gtk-theme papirus-icon-theme --noconfirm --needed
 sudo pacman -S pulseaudio pulseaudio-alsa --noconfirm --needed
 # qt applications
 sudo pacman -S kio --noconfirm --needed
+# qemu & virtual-manager
+sudo pacman -S qemu libvirt ebtables dnsmasq virt-manager --noconfirm --needed
+sudo systemctl enable libvirtd.service ebtables.service dnsmasq.service
