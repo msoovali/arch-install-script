e9f5008592daed9547561d7740e78b24ed6d03a3
Martin Sooväli <msoovali@gmail.com>
2019-07-06 18:53:28 +0300

    Add more usable packages

--- arch-postinstall-script.sh
+++ arch-postinstall-script.sh
@@ -7,16 +7,20 @@ sudo pacman -S xorg-server xorg-apps xorg-xinit xf86-video-intel xf86-video-nouv
 sudo pacman -S lightdm lightdm-gtk-greeter --noconfirm --needed
 sudo systemctl enable lightdm.service
 # graphical indicator for networkmanager (not needed for cinnamon/gnome)
-#sudo pacman -S network-manager-applet --noconfirm --needed
+sudo pacman -S network-manager-applet --noconfirm --needed
 # cinnamon desktop environment
-sudo pacman -S cinnamon gnome-terminal gedit eog nemo-fileroller gnome-screenshot gnome-calculator --noconfirm --needed
+#sudo pacman -S cinnamon gnome-terminal gedit eog nemo-fileroller gnome-screenshot gnome-calculator --noconfirm --needed
 # mate desktop environment
-#sudo pacman -S mate mate-extra --noconfirm --needed
+sudo pacman -S mate mate-extra --noconfirm --needed
+# user directories
+sudo pacman -S xdg-user-dirs-gtk --noconfirm --needed
+# ssh
+sudo pacman -S openssh --noconfirm --needed
 # printing service
 sudo pacman -S cups cups-pdf system-config-printer --noconfirm --needed
 sudo systemctl enable org.cups.cupsd.service
 # some fonts
-sudo pacman -S adobe-source-sans-pro-fonts cantarell-fonts noto-fonts terminus-font ttf-bitstream-vera ttf-dejavu ttf-droid ttf-inconsolata ttf-liberation ttf-roboto ttf-ubuntu-font-family tamsyn-font --noconfirm --needed
+sudo pacman -S adobe-source-sans-pro-fonts cantarell-fonts noto-fonts noto-fonts-emoji terminus-font ttf-bitstream-vera ttf-dejavu ttf-droid ttf-inconsolata ttf-liberation ttf-roboto ttf-ubuntu-font-family tamsyn-font --noconfirm --needed
 # some useful applications
 sudo pacman -S firefox gimp audacity transmission-gtk vlc keepassxc libreoffice-still htop code --noconfirm --needed
 # wireless driver
@@ -25,8 +29,8 @@ sudo pacman -S broadcom-wl --noconfirm --needed
 sudo pacman -S arc-gtk-theme papirus-icon-theme --noconfirm --needed
 # audio
 sudo pacman -S pulseaudio pulseaudio-alsa --noconfirm --needed
-# qt applications
-sudo pacman -S kio --noconfirm --needed
+# qt applications and other
+sudo pacman -S kio gconf --noconfirm --needed
 # qemu & virtual-manager
 sudo pacman -S qemu libvirt ebtables dnsmasq virt-manager --noconfirm --needed
 sudo systemctl enable libvirtd.service ebtables.service dnsmasq.service
