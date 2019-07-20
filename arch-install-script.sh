65a464acc121a57247785f6317f498ed6eb1df35
Martin Sooväli <msoovali@gmail.com>
2019-07-20 15:51:47 +0300

    Add locale.conf copy to /etc

--- arch-install-script.sh
+++ arch-install-script.sh
@@ -22,7 +22,8 @@ hwclock --systohc
 sed -i 's/#en_US.UTF-8\ UTF-8/en_US.UTF-8\ UTF-8/' /etc/locale.gen
 sed -i 's/#et_EE.UTF-8\ UTF-8/et_EE.UTF-8\ UTF-8/' /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+cp locale.conf /etc/
 
 echo "$hostname" > /etc/hostname
 
