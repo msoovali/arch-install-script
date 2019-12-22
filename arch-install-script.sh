e01d8eab52f4a19d727ea93121dcf0e98d0e95a6
Martin Sooväli <msoovali@gmail.com>
2019-12-22 12:55:32 +0200

    Add gpt setup with systemd-boot

--- arch-install-script.sh
+++ arch-install-script.sh
@@ -4,16 +4,26 @@
 # timedatectl set-ntp true
 # lsblk - list available disks
 # cfdisk /dev/sdX - partition selected disk
+#
+# -- DOS --
 # mkfs.ext4 /dev/sdX1 - format partition
 # mount /dev/sdX1 /mnt
-# pacstrap /mnt base base-devel
+# -- GPT --
+# mkfs.ext4 /dev/sda2
+# mkfs.vfat /dev/sda1
+# mount /dev/sda2 /mnt
+# mkdir /mnt/boot
+# mount /dev/sda1 /mnt/boot
+#
+# pacstrap /mnt base base-devel linux linux-firmware nano networkmanager bash-completion intel-ucode
 # genfstab -U /mnt >> /mnt/etc/fstab
 # arch-chroot /mnt
 
 echo "Enter device's hostname:"
 read -r hostname
 
-echo "Enter disk path (/dev/sda)"
+# echo "Enter disk path (/dev/sda)" <-- DOS
+echo "Enter Linux filesystem path (/dev/sda2)"
 read -r d_path
 
 ln -sf /usr/share/zoneinfo/Europe/Tallinn /etc/localtime
@@ -33,7 +43,15 @@ cat > /etc/hosts <<EOF
 127.0.1.1 $hostname.localdomain	$hostname
 EOF
 
-pacman -S --noconfirm grub bash-completion networkmanager
+# DOS
+#pacman -S --noconfirm grub
+#grub-install $d_path
+#grub-mkconfig -o /boot/grub/grub.cfg
 
-grub-install $d_path
-grub-mkconfig -o /boot/grub/grub.cfg
+# GPT
+bootctl --path=/boot install
+cp loader.conf /boot/loader/
+cp arch.conf /boot/loader/entries/
+uuid_of_partition=$(blkid $d_path -o value UUID)
+sed -i "s/MY_UUID/$uuid_of_partition/" /boot/loader/entries/arch.conf
+
